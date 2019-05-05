c7712fbc760d6924fa89b0fea41864f32baef3da
Christopher Chin <christopher@Monicas-iMac.local>
2019-05-05 22:36:16 +1000

    Contact Component add to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Component }from "react";
+import React, { Component }from 'react';
+import Contact from './components/Contact'; 
+
 import "./App.css";
 
 class App extends Component {
   render(){
-    return React.createElement(
-      'div',
-      { className: 'App'},
-      React.createElement('h1'),null, 'The App Component')
+
+    return(
+    <div className="App">
+      <h1>The App Component</h1>
+      <Contact/>
+      <Contact/>
+    </div>
+    );
   }
 }
 
